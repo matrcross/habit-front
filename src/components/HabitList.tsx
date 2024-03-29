8f70f1c40f94e97f9376580e7cc76e82f31f7939
Mat <mt.ribeiro4@gmail.com>
2024-03-29 19:24:21 -0300

    feat: added dynamic habitList and a way of add and remove habits

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,5 +1,5 @@
 // import WeekDaysForAsign from "./WeekDaysForAsign"
-import {ChangeEvent} from "react";
+import {ChangeEvent, KeyboardEvent, useState} from "react";
 
 /*
   Tornar os outros editaveis
@@ -10,15 +10,78 @@ import {ChangeEvent} from "react";
 */
 
 function WeekContainer() {
+  const [habitList, setHabitList] = useState([
+    {
+      id: 0,
+      name:'Routine A',
+      asignAt:''
+    },
+    {
+      id: 1,
+      name:'Routine B',
+      asignAt:''
+    },
+  ])
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const target = e.target
     target.classList.toggle('show-after', target.textContent?.trim() == '')
   };
 
+  const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>, habitId: number) =>{
+    const focusedElement = e.target as HTMLElement;
+
+    if(e.key == 'Backspace' 
+    && focusedElement.textContent == ''
+    && habitId != 0
+    ){
+      const updatedHabitList = habitList.filter(habit => habit.id !== habitId);
+      setHabitList(updatedHabitList);
+      const aboveHabit = document.getElementById(`habit-${habitId-1}`)
+      aboveHabit?.focus()
+    }
+
+    if(e.key == 'Enter'){
+      e.preventDefault();
+      if(habitId == (habitList.length-1)){
+        const emptyHabit = {
+          id: habitId+1,
+          name: '',
+          asignAt: ''
+        }
+  
+        setHabitList([...habitList, emptyHabit])
+        setTimeout(() => {
+          const newlyCreatedDiv = document.getElementById(`habit-${emptyHabit.id}`);
+          if (newlyCreatedDiv) {
+            newlyCreatedDiv.classList.add('show-after');
+            newlyCreatedDiv.focus()
+          }
+        }, 0);
+      }
+      document.getElementById(`habit-${habitId+1}`)?.focus()
+      }
+      
+  }
+
+
   return (
     <div>
       <h1>Habits</h1>
       <div className="habit-list-test">
+        {habitList.map((habit, index)=>(
+          <div 
+          key={habit.id}
+          id={`habit-${habit.id}`}
+            onInput={handleChange}
+            className='editable-div' 
+            contentEditable="true"
+            onKeyDown={(event) => handleKeyDown(event, index)}>
+              {habit.name}
+            </div>
+        ))}
+      </div>
+      {/* <div className="habit-list-test">
         <div 
         onInput={handleChange}
         className='editable-div show-after' 
@@ -29,8 +92,7 @@ function WeekContainer() {
         className='editable-div show-after'  
         contentEditable="true">
         </div>
-
-      </div>
+      </div> */}
     </div>
   )
 }
